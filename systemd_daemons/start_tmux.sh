2d6451d3c587d4c4f3ef24c026263190df4a1abe
Cristian Porzio <38082151+panuozzo77@users.noreply.github.com>
2024-10-25 11:31:22 +0200

    Create start_tmux.sh

---
+++ systemd_daemons/start_tmux.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Log all output
+exec > /tmp/tmux-session.log 2>&1
+
+# Create the tmux session with the first window (index 0)
+tmux new-session -s mysession -n bash -d
+tmux send-keys -t mysession:0 'cd ~' C-m
+
+# Create the factorio window (index 1)
+tmux new-window -t mysession:1 -n factorio
+tmux send-keys -t mysession:1 'cd /home/criadmin/mountedDrives/SSD/factorio_vanilla_2024_10 && ./start.sh' C-m
+
+# Select the first window (bash)
+tmux select-window -t mysession:0
